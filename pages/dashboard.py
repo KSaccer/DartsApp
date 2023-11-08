4e9e57eec17ff58b1ffd12fc4ada0c9ba60180ff
KSaccer <sankis87@gmail.com>
2023-11-08 23:34:32 +0100

    project structure update

---
+++ pages/dashboard.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class Dashboard(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        tk.Label(self, text="DASHBOARD", background="lightsteelblue").grid(
+            row=0, column=0, sticky="news")
+        
+if __name__ == "__main__":
+    pass
