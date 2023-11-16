e19dbdb8d9c5d8ad5d4ac33fad8b4885f18459aa
KSaccer <sankis87@gmail.com>
2023-11-16 23:59:19 +0100

    Minor GUI update

--- pages/statistics.py
+++ pages/statistics.py
@@ -22,21 +22,17 @@ class StatPage(ttk.Frame):
         super().__init__(parent, *args, **kwargs)
         self.rowconfigure((0, 1, 2), weight=1)
         self.columnconfigure(0, weight=1)
-        # tk.Label(self, text="STATISTICS", background="lightsteelblue").grid(
-        #     row=0, column=0, sticky="news")
         self.create_gui()
-        
     
     def create_gui(self) -> None:
         self.page_title = PageTitle(self)
         self.page_title.grid(row=0, column=0)
         
         self.plot_selector = PlotSelector(self)
-        self.plot_selector.grid(row=1, column=0)
+        self.plot_selector.grid(row=1, column=0, sticky="news")
 
         self.plot = Plot(self)
-        self.plot.grid(row=2, column=0)
-
+        self.plot.grid(row=2, column=0, sticky="news")
         
 
 class PageTitle(ttk.Frame):
@@ -51,17 +47,23 @@ class PlotSelector(ttk.Frame):
     def __init__(self, parent, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
         self.rowconfigure(0, weight=1)
-        self.columnconfigure((0, 1), weight=1)
+        self.columnconfigure((0, 1, 2, 3), weight=1)
+
+        plot_type_label = ttk.Label(self, text="Plot Type: ")
+        plot_type_label.grid(row=0, column=0, sticky="e")
 
         plot_type = ttk.Combobox(self)
         plot_type["values"] = ["Averages", "Nr of Sessions"]
         plot_type["state"] = "readonly"
-        plot_type.grid(row=0, column=0, sticky="n")
-        
+        plot_type.grid(row=0, column=1, sticky="w")
+
+        time_scale_label = ttk.Label(self, text="Plot Type: ")
+        time_scale_label.grid(row=0, column=2, sticky="e")
+
         time_scale = ttk.Combobox(self)
         time_scale["values"] = ["Monthly", "Yearly"]
         time_scale["state"] = "readonly"
-        time_scale.grid(row=0, column=1, sticky="n")
+        time_scale.grid(row=0, column=3, sticky="w")
 
 
 class Plot(ttk.Frame):
@@ -71,9 +73,7 @@ class Plot(ttk.Frame):
         self.fig = self.create_plot()
         canvas = FigureCanvasTkAgg(self.fig, master=self)
         canvas.draw()
-        canvas.get_tk_widget().grid(row=0, column=0)
-
-
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def create_plot(self) -> Figure:
         # Read in data
@@ -88,32 +88,28 @@ class Plot(ttk.Frame):
         df_monthly_av = df_val_res.sum() / df_val_res.count() * 3
 
         # Setting up plot layout
-        fig, axs = plt.subplots(2, 1, figsize=(6,5))
+        fig, ax = plt.subplots(1, 1, figsize=(6,5))
         fig.suptitle("Monthly statistics", fontsize=20, fontweight="normal", y=1.0)
         sns.set_theme(style="whitegrid")
         fig.tight_layout(h_pad=5)
 
         # Creating plots
-        axs[0].bar(df_monthly_tr.index, df_monthly_tr.round_nr, width=15, color="tab:blue",  edgecolor='black')
-        sns.lineplot(x=df_monthly_av.index, y=df_monthly_av.value, color="tab:blue", marker='o', ax=axs[1])
+        sns.lineplot(x=df_monthly_av.index, y=df_monthly_av.value, color="tab:blue", marker='o', ax=ax)
 
         # Formatting
         years, months  = mdates.YearLocator(), mdates.MonthLocator()   # every year
         years_format, months_format = mdates.DateFormatter('%Y'), mdates.DateFormatter('%m')
 
-        axs[0].set(title="Nr of rounds per month", xlabel="Date", ylabel="Nr of rounds")
-        axs[1].set(title="Monthly Average", xlabel="Date", ylabel="3-Dart Average")
-
-        for ax in axs:
-            ax.set_axisbelow(True)
-            ax.xaxis.grid(color='gray', linestyle='dashed')
-            ax.yaxis.grid(color='gray', linestyle='dashed')
-            ax.xaxis.set_major_locator(years)
-            ax.xaxis.set_major_formatter(years_format)
-            ax.xaxis.set_minor_locator(months)
-            ax.xaxis.set_minor_formatter(months_format)
-            for label in ax.get_xticklabels():
-                label.set_rotation(90)
+        ax.set(title="Monthly Average", xlabel="Date", ylabel="3-Dart Average")
+        ax.set_axisbelow(True)
+        ax.xaxis.grid(color='gray', linestyle='dashed')
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        ax.xaxis.set_major_locator(years)
+        ax.xaxis.set_major_formatter(years_format)
+        ax.xaxis.set_minor_locator(months)
+        ax.xaxis.set_minor_formatter(months_format)
+        for label in ax.get_xticklabels():
+            label.set_rotation(90)
         return fig
     
         
