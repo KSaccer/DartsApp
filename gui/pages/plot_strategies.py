e36f60a7bf5bee4574c8f475a0171ae4a12a7564
KSaccer <sankis87@gmail.com>
2024-01-16 22:10:42 +0100

    PlotStrategies moved into a separate file

---
+++ gui/pages/plot_strategies.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+import os
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+import sqlite3
+import seaborn as sns
+from abc import ABC, abstractmethod
+from matplotlib.figure import Figure, Axes
+
+
+class PlotStrategy(ABC):
+    """Strategy Interface for plot builder algorithms"""
+
+    @abstractmethod
+    def build_plot(self, db: DataBase) -> (Figure, Axes):
+        pass
+
+    @abstractmethod
+    def _create_df(self, db: DataBase, sql_script: str) -> None:
+        pass
+
+    @abstractmethod
+    def _create_plot_content(self, df: pd.DataFrame) -> (Figure, Axes):
+        pass
+
+    @abstractmethod
+    def _format_plot_content(self, fig: Figure, ax: Axes) -> (Figure, Axes):
+        pass
+
+
+class ThreeDartAvg(PlotStrategy):
+    """Strategy for three dart average plot"""
+
+    sql_script = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 
+        "sql", 
+        "avg.sql")
+    
+    def build_plot(self, db: DataBase, sampling_rule: str) -> (Figure, Axes):
+        """Execute plot builder process"""
+        # Create DataFrame
+        df = self._create_df(db, ThreeDartAvg.sql_script, sampling_rule)
+        fig, ax = self._create_plot_content(df)
+        fig, ax = self._format_plot_content(fig, ax)
+        return (fig, ax)
+      
+    def _create_df(self, db: DataBase, sql_script: str, sampling_rule: str) -> pd.DataFrame:
+        """Create the DataFrame by connecting to the database and running
+        the SQL script"""
+        conn = sqlite3.connect(db.db_path)
+        with open(sql_script, "r") as query:
+            df = pd.read_sql_query(query.read(), conn, 
+                                   parse_dates={"date": {"format": "%Y-%m-%d"}})
+        df = df.set_index("date")
+        df = df.resample(sampling_rule).sum()
+        conn.close()
+        return df
+    
+    def _create_plot_content(self, df: pd.DataFrame) -> (Figure, Axes):
+        """Set up plot and create curves"""
+        # Figure setup
+        fig, ax = plt.subplots(1, 1)
+        fig.tight_layout(h_pad=5)
+        # Creating plot
+        sns.scatterplot(x=df.index, y=df.overall_score / df.visits, 
+                        color="tab:blue", marker='o', ax=ax)
+        sns.lineplot(x=df.index, y=df.overall_score / df.visits, 
+                        color="lightgray", ax=ax)
+        ax.lines[0].set_linestyle("--")
+        try:
+            df_smooth = df.resample("D").interpolate(method="quadratic")
+        except ValueError:
+            df_smooth = df
+        sns.lineplot(x=df_smooth.index, 
+                        y=df_smooth.overall_score / df_smooth.visits, 
+                        color="tab:orange", ax=ax
+                        )
+        return (fig, ax)
+
+    def _format_plot_content(self, fig, ax) -> (Figure, Axes):
+        """Format plot axes and appearance"""
+        years, months  = mdates.YearLocator(), mdates.MonthLocator()   # every year
+        years_format = mdates.DateFormatter('%Y')
+        months_format = mdates.DateFormatter('%m')
+        ax.set_axisbelow(True)
+        ax.xaxis.grid(color='lightgray', linestyle='dashed')
+        ax.yaxis.grid(color='lightgray', linestyle='dashed')
+        ax.xaxis.set_major_locator(years)
+        ax.xaxis.set_major_formatter(years_format)
+        ax.xaxis.set_minor_locator(months)
+        ax.xaxis.set_minor_formatter(months_format)
+        for label in ax.get_xticklabels():
+            label.set_rotation(90)
+        return (fig, ax)
+    
+
+class NrOfSessions(PlotStrategy):
+    """Strategy for bar chart showing the nr of session played"""
+
+    sql_script = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 
+        "sql", 
+        "nr_of_games.sql")
+    
+    def build_plot(self, db: DataBase, sampling_rule: str) -> (Figure, Axes):
+        """Execute plot builder process"""
+        # Create DataFrame
+        df = self._create_df(db, NrOfSessions.sql_script, sampling_rule)
+        fig, ax = self._create_plot_content(df)
+        fig, ax = self._format_plot_content(fig, ax)
+        return (fig, ax)
+      
+    def _create_df(self, db: DataBase, sql_script: str, sampling_rule: str) -> pd.DataFrame:
+        """Create the DataFrame by connecting to the database and running
+        the SQL script"""
+        conn = sqlite3.connect(db.db_path)
+        with open(sql_script, "r") as query:
+            df = pd.read_sql_query(query.read(), conn, 
+                                   parse_dates={"date": {"format": "%Y-%m-%d"}})
+        df = df.set_index("date")
+        df = df.resample(sampling_rule).sum()
+        conn.close()
+        return df
+
+    def _create_plot_content(self, df: pd.DataFrame) -> (Figure, Axes):
+        """Set up plot and create curves"""
+        # Figure setup
+        fig, ax = plt.subplots(1, 1)
+        fig.tight_layout(h_pad=5)
+        # Creating plot
+        ax.bar(df.index, df.nr_of_games, width=15, 
+               color="tab:blue",  edgecolor='darkblue')
+        return (fig, ax)
+
+    def _format_plot_content(self, fig, ax) -> (Figure, Axes):
+        """Format plot axes and appearance"""
+        years, months  = mdates.YearLocator(), mdates.MonthLocator()   # every year
+        years_format = mdates.DateFormatter('%Y')
+        months_format = mdates.DateFormatter('%m')
+        ax.set_axisbelow(True)
+        ax.xaxis.grid(color='lightgray', linestyle='dashed')
+        ax.yaxis.grid(color='lightgray', linestyle='dashed')
+        ax.xaxis.set_major_locator(years)
+        ax.xaxis.set_major_formatter(years_format)
+        ax.xaxis.set_minor_locator(months)
+        ax.xaxis.set_minor_formatter(months_format)
+        for label in ax.get_xticklabels():
+            label.set_rotation(90)
+        return (fig, ax)
+    
+
+if __name__ == "__main__":
+    pass
